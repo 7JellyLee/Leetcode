142d3319ae92120a848f38f71b49cb27b62d7205
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-24 17:27:25 +0800

    Create 226.翻转二叉树.py
    1023 翻转二叉树

---
+++ 191023/226.翻转二叉树.py
@@ -0,0 +1,13 @@
+#Leetcode
+#226.翻转二叉树
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root != None:
+            node = root
+            node.left, node.right = self.invertTree(node.right), self.invertTree(node.left)
+        else:
+            return None
+
+        return node
