afff188ed41fb585091f402cf12b3ac7bcb7cbb0
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-25 13:36:49 +0800

    ADD

---
+++ 191023/235.二叉搜索树的最近公共祖先.py
@@ -0,0 +1,7 @@
+#Leetcode
+#235.二叉搜索树的最近公共祖先
+#一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+            
