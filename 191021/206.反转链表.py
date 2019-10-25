941d9abcefa3923e984f02d84e5b3fb8298ad13b
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-25 18:37:06 +0800

    Update 206.反转链表.py
    提交算法

--- 191021/206.反转链表.py
+++ 191021/206.反转链表.py
@@ -2,16 +2,17 @@
 #206.反转链表
 #反转一个单链表
 
+#本题关键点、
+#1.链表新建头结点 dummy = None
+#2.翻转链表需要三个指针，注意翻转时转换的顺序
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         dummy = None
         p = head
-        
-        while p:
+
+        while p is not None:
             r = p.next
             p.next = dummy
-            dummy = p
             p = r
-        
-        return dummy
             
+        return dummy
