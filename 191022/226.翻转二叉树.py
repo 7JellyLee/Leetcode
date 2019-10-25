6229535f826b614e35185136e6258d5bb9d5e834
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-25 19:16:47 +0800

    提交翻转二叉树的算法内容

--- 191022/226.翻转二叉树.py
+++ 191022/226.翻转二叉树.py
@@ -2,13 +2,18 @@
 #226.翻转二叉树
 #反转一棵二叉树
 
+#本题关键点
+#递归思想
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        if root != None:
-            node = root
-            node.left, node.right = self.invertTree(node.right), self.invertTree(node.left)
-        else:
+        if root is None:
             return None
-
+        else:
+            node = root
+            tmp = node.left
+            node.left = self.invertTree(node.right)
+            node.right = self.invertTree(tmp)
         return node
+
+
