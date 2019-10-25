f88e3cd46b77009ca961bf945655902636f510ff
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-25 13:48:33 +0800

    Create 237.删除链表中的节点.py
    添加237删除链表节点的题目

---
+++ 191025/237.删除链表中的节点.py
@@ -0,0 +1,10 @@
+#Leetcode
+#237.删除链表中的节点
+#删除某个链表中给定的节点
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
