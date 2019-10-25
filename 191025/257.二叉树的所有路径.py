adf4b656efe6440691202e6eed3c2f7fac10a935
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-25 13:49:25 +0800

    Create 257.二叉树的所有路径.py
    添加257二叉树所有路径的题目

---
+++ 191025/257.二叉树的所有路径.py
@@ -0,0 +1,13 @@
+#Leetcode
+#257.二叉树的所有路径
+#给定一个二叉树，返回所有从根节点到叶子节点的路径
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
