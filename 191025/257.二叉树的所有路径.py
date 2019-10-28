502ab7dfcbefe459dd4b2955784dd6f2d6d5060f
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-28 22:15:22 +0800

    Update 257.二叉树的所有路径.py
    添加257 二叉树的所有路径的算法代码

--- 191025/257.二叉树的所有路径.py
+++ 191025/257.二叉树的所有路径.py
@@ -2,12 +2,23 @@
 #257.二叉树的所有路径
 #给定一个二叉树，返回所有从根节点到叶子节点的路径
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#本题关键点
+#1.使用递归，如果是叶子节点，就放在路径结尾，并返回答案。
+# 如果不是叶子节点就递归
 class Solution:
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if root:
+            path += str(root.val)
+            if not root.left and not root .right:
+                path.append(path)
+            else:
+                path += '->'
+                construct_paths(root.left,path)
+                construct_paths(root.right,path)
+
+        path = []
+        construct paths(root,'')
+        return path
+
+
+
