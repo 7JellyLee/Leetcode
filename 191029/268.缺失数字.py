73e0d3305d3601f2976a10990befff71bd7d38c0
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-29 19:45:39 +0800

    Update 268.缺失数字.py
    添加268缺失数字算法代码内容

--- 191029/268.缺失数字.py
+++ 191029/268.缺失数字.py
@@ -3,6 +3,11 @@
 #给定一个包含0，1，2，，，，n的序列，找出没出现的那个数
 
 #本题关键点
-
+# 1.
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in numSet:
+                return number
