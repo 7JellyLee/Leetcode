457bb68eacf90ba40609f2ea7df225c0698ed15d
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-30 20:02:44 +0800

    Update 283.移动零.py
    添加283移动零的算法代码内容

--- 191030/283.移动零.py
+++ 191030/283.移动零.py
@@ -3,9 +3,20 @@
 # 编写一个函数，将数组nums中的0全部一定到数组的末尾，同时保持非零元素的相对位置
 
 #本题关键点
-#1.
+#1.遍历数组，k记录
+# 2.数组多个赋值 nums[k:] = [0] * (个数)
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+         k = -1
+
+            for num in nums:
+                if num :
+                    k += 1
+                    nums[k] = num
+                else:
+                    continue
+
+            if k == len(nums):
+                return nums
+            else:
+                nums[k+1:] = [0] * (len(nums) - k - 1)
