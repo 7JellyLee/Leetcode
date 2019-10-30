d9cde3148b1b1aa4ff32801af65db53814f7aa3e
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-30 13:46:20 +0800

    添加191030算法题目内容

---
+++ 191030/283.移动零.py
@@ -0,0 +1,11 @@
+#Leetcode
+#283.移动零
+# 编写一个函数，将数组nums中的0全部一定到数组的末尾，同时保持非零元素的相对位置
+
+#本题关键点
+#1.
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
---
+++ 191030/290.单词规律.py
@@ -0,0 +1,8 @@
+#Leetcode
+#290，单词规律
+#给定一种规律pattern和一个字符串str，判断str是否完全匹配相同的规律
+
+#本题关键点
+#1
+class Solution:
+    def wordPattern(self, pattern: str, str: str) -> bool:
