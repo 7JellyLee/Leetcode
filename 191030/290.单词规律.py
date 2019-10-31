4dc02be52002ed638ed42944906c189ab0d9f17c
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-10-31 16:11:26 +0800

    Update 290.单词规律.py
    添加290单词规律算法代码内容

--- 191030/290.单词规律.py
+++ 191030/290.单词规律.py
@@ -6,3 +6,25 @@
 #1
 class Solution:
     def wordPattern(self, pattern: str, str: str) -> bool:
+        words = str.split(" ")
+        hash_p = {}
+        hash_w = {}
+
+        if len(words) != len(pattern):
+            return False
+
+        for i,letter in enumerate(pattern):
+            if hash_p.get(letter):
+                if hash_p[letter] != words[i]:
+                    return  False
+            else:
+                hash_p[letter] = words[i]
+
+        for i, word in enumerate(words):
+            if hash_w.get(word):
+                if hash_w[word] != pattern[i]:
+                    return False
+            else:
+                hash_w[word] = pattern[i]
+
+        return True
