a5bdc620ea9c5efb1c34a8cc61c5a4d06f1bacfd
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-11-02 20:41:19 +0800

    Update 299.猜数字游戏.py
    添加猜数字游戏算法代码内容

--- 191031/299.猜数字游戏.py
+++ 191031/299.猜数字游戏.py
@@ -5,17 +5,16 @@
 #本题关键点
 #1.
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-         k = -1
-
-            for num in nums:
-                if num :
-                    k += 1
-                    nums[k] = num
-                else:
-                    continue
-
-            if k == len(nums):
-                return nums
-            else:
-                nums[k+1:] = [0] * (len(nums) - k - 1)
+    def getHint(self, secret: str, guess: str) -> str:
+        from collections import Counter
+        s_c = Counter(secret)
+        g_c = Counter(guess)
+        res =  [0, 0]
+        for x, y in zip(secret, guess):
+            if x == y:
+                s_c[x] -= 1
+                g_c[y] -= 1
+                res[0] += 1
+        for k in s_c & g_c:
+            res[1] += min(s_c[k], g_c[k])
+        return "{}A{}B".format(res[0], res[1])
