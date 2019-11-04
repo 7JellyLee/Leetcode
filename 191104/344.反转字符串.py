61e1cb6456ce1444c728564fdb9c7e4c732700e2
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-11-04 12:46:27 +0800

    Update 344.反转字符串.py
    添加344反转字符串算法代码内容

--- 191104/344.反转字符串.py
+++ 191104/344.反转字符串.py
@@ -3,18 +3,18 @@
 #原地修改输入数组，将输入的字符串反转过来
 
 #本题关键点
-#1.
+#1. 本题使用双指针交换
 class Solution:
-    def getHint(self, secret: str, guess: str) -> str:
-        from collections import Counter
-        s_c = Counter(secret)
-        g_c = Counter(guess)
-        res =  [0, 0]
-        for x, y in zip(secret, guess):
-            if x == y:
-                s_c[x] -= 1
-                g_c[y] -= 1
-                res[0] += 1
-        for k in s_c & g_c:
-            res[1] += min(s_c[k], g_c[k])
-        return "{}A{}B".format(res[0], res[1])
+    def reverseString(self, s: List[str]) -> None:
+        l = len(s)
+        if l < 2:
+            return s
+
+        j = l - 1
+        i = 0
+
+        while i < j:
+            s[i], s[j] = s[j],s[i]
+            i += 1
+            j -= 1
+        return s
