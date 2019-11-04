c6b1a0d134243143386e5854cd10cc5bcea55cc9
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-11-04 12:40:04 +0800

    Update 342.4的幂.py
    添加342 4的幂算法代码内容

--- 191104/342.4的幂.py
+++ 191104/342.4的幂.py
@@ -5,18 +5,18 @@
 
 
 #本题关键点
-#1
+#1.方法一本题和上题判断3的幂是一样的，循环迭代
+# 2.使用位运算，4的幂一定是2的幂，4的幂和2的幂一样，都是只会出现一个1，4的幂总是出现在奇数位上
+#  使用num & 0x55555555方式来校验奇数位上的1
 
-class NumArray:
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
 
-    def __init__(self, nums: List[int]):
-        self.nums = nums
-        for i in range(len(self.nums)):
-            if i > 0:
-                self.nums[i] = self.nums[i]+self.nums[i-1]
+        while num != 0 and num % 4 == 0:
+            num //= 4
 
-    def sumRange(self, i: int, j: int) -> int:
-        if i>0:
-            return self.nums[j]-self.nums[i-1]
-        else:
-            return self.nums[j]
+        return num == 1
+
+
+    def isPowerOfFour(self, num: int) -> bool:
+        return num>0 and (num & num - 1)==0 and (num & 0x55555555)==num
