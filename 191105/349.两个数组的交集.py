1aad72c7dab5ae2eb1c23827e9d15df5895bac02
7JellyLee <35484459+7JellyLee@users.noreply.github.com>
2019-11-06 10:58:04 +0800

    Update 349.两个数组的交集.py
    添加349两个数组交集算法代码内容

--- 191105/349.两个数组的交集.py
+++ 191105/349.两个数组的交集.py
@@ -1,17 +1,12 @@
 #Leetcode
 #349.两个数组的交集
 
-#给定一个整数判断是否是3的幂次方
+#给定两个数组，编写一个函数来计算他们的交集
 
 #本题关键点
-# 1.迭代循环，只要数字对3除余一直为 0 ，就将n对3取整数
+# 1.python操作 x & y交集
+# 2.list函数将元组转化为列表
+# 3.set无序不重复函数
 class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-
-        if n <= 0:
-            return False
-
-        while n % 3 == 0:
-            n //= 3
-
-        return n == 1
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
