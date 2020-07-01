4be5d40d66d6d36f8ffc3d7df26ad12561334314
lijingbo <906139088@qq.com>
2020-07-01 20:30:17 +0800

    7月1号任务提交

--- 200701/1.两数之和.js
+++ 200701/1.两数之和.js
@@ -13,5 +13,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
+    const map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i]
+        }
+        map.set(nums[i], i)
+    }
 };
