aff18581701385b0a99b4a22acbcf31876a488ae
lijingbo <906139088@qq.com>
2020-07-01 14:12:04 +0800

    js文件
    添加js

---
+++ 200701/1.两数之和.js
@@ -0,0 +1,17 @@
+// #Leetcode
+// #1.两数之和
+
+// #给定一个整数数组nums和一个目标值target，在该数组中找出和为目标值的那两个整数，返回他们的数组下标。
+
+// #本题关键点
+// #1.使用前后双指针，如果指针指向的字母不是元音字母前指针后移后指针前移，当两者都是愿元音字母时交换
+// # 2.元音字母要注意有大写字母的情况
+// # 3.python中可以不用设置临时变量直接将两者交换 x,y = y,x
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+};
