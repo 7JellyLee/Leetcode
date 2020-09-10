534201ec8c82382dcc91c5c15d2d9e545ab1724c
lijingbo <906139088@qq.com>
2020-09-10 21:25:01 +0800

    9号上传

--- 200909/jz40数组中只出现一次的数字.py
+++ 200909/jz40数组中只出现一次的数字.py
@@ -1,33 +1,13 @@
 #题目描述
-#输入两个链表，找出它们的第一个公共结点。（注意因为传入数据是链表，所以错误测试数据的提示是用其他方式显示的，保证传入数据是正确的）
+#输入一棵二叉树，判断该二叉树是否是平衡二叉树。
 class Solution:
-    def FindFirstCommonNode(self, pHead1, pHead2):
+    # 返回[a,b] 其中ab是出现一次的两个数字
+    def FindNumsAppearOnce(self, array):
         # write code here
-        len1 = 0
-        len2 = 0
+        res = []
 
-        p1 = pHead1
-        p2 = pHead2
-
-        while p1:
-            len1 += 1
-        while p2 :
-            len2 += 1
-
-        if len1 > len2:
-            long = pHead1
-        else:
-            short = pHead1
+        for i in range(len(array)):
+            if array.count(i) == 1:
+                res.append(i)
         
-        diff = abs(len1 - len2)
-
-        for i in range(diff):
-            long = long.next
-
-        while long:
-            if long == short:
-                return long
-            long = long.next
-            short = short.next
-
-        return None
+        return res
